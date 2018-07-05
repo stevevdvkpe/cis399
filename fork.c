9eecd7162e63bbfa8cc3777088215a8ef1084c31
Steve VanDevender <stevev@hexadecimal.uoregon.edu>
2018-07-05 15:04:48 -0700

    add comments to fork.c

--- fork.c
+++ fork.c
@@ -7,11 +7,13 @@ int main(int argc, char *argv[])
 	int pid, status;
 
 	if ((pid = fork()) == 0) {
+		/* fork returns 0 in child */
 		printf("in child: pid is %d, parent pid is %d\n",
 		       getpid(), getppid());
 		exit(0);
 	}
 	else {
+		/* fork returns pid to parent */
 		printf("in parent: pid is %d, child pid is %d\n",
 		       getpid(), pid);
 		pid = wait(&status);
